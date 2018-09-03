d7a44415c959bafae4bbe945a55f1954683aede7
Claudio De Angelis <claudio.deangelis@cloud-iq.com>
2018-09-03 22:25:49 +1000

    finished formatting minified json

--- src/Components/Controllers/Background.js
+++ src/Components/Controllers/Background.js
@@ -36,11 +36,18 @@ class Background extends React.Component {
         if(json){
             let html = JSON.stringify(json);
             html = html.replace(/("|{"|,")([^"]+)(":)/gm,`$1<span class='json-key'>$2</span>$3`);
-            html = html.replace(/(:"|\[")([^"]+)(")/gm,`$1<span class='json-string'>$2</span>$3`);
-            html = html.replace(/(\[")([^"]+)(",|"\]|",")/gm,`$1<span class='json-string'>$2</span>$3`);
-            html = html.replace(/(,")([^"]+)(",|"])/gm,`$1<span class='json-string'>$2</span>$3`);
+            html = html.replace(/(:|\[)(")([^"]+)(")/gm,`$1<span class='json-string'>$2$3$4</span>`);
+            html = html.replace(/(\[)(")([^"]+)(")(,|\]|,")/gm,`$1<span class='json-string'>$2$3$4</span>$5`);
+            html = html.replace(/(,)(")([^"]+)(")(,|\])/gm,`$1<span class='json-string'>$2$3$4</span>$5`);
             html = html.replace(/(,|:\[|:{|:)(\d+)(,|\]|})/gm,`$1<span class='json-value'>$2</span>$3`);//
             html = html.replace(/(,|:\[|:{|:)(\d+)(])/gm,`$1<span class='json-value'>$2</span>$3`);
+            ///
+            // html = html.replace(/("|{"|,")([^"]+)(":)/gm,`$1<span class='json-key'>$2</span>$3`);
+            // html = html.replace(/(:"|\[")([^"]+)(")/gm,`$1<span class='json-string'>$2</span>$3`);
+            // html = html.replace(/(\[")([^"]+)(",|"\]|",")/gm,`$1<span class='json-string'>$2</span>$3`);
+            // html = html.replace(/(,")([^"]+)(",|"])/gm,`$1<span class='json-string'>$2</span>$3`);
+            // html = html.replace(/(,|:\[|:{|:)(\d+)(,|\]|})/gm,`$1<span class='json-value'>$2</span>$3`);//
+            // html = html.replace(/(,|:\[|:{|:)(\d+)(])/gm,`$1<span class='json-value'>$2</span>$3`);
             return html;
         }
         else return '';
