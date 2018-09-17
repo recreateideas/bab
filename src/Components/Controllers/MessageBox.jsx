21fdfdb4115112e0cdd32d5f88fb12815d8cfed0
Claudio De Angelis <claudio.deangelis@cloud-iq.com>
2018-09-17 14:16:38 +1000

    formatted chat baubbles

--- src/Components/Controllers/MessageBox.jsx
+++ src/Components/Controllers/MessageBox.jsx
@@ -4,10 +4,12 @@ import PropTypes from 'prop-types';
 class MessageBox extends React.Component{
 
     renderMessage(message,key){
-        const messageTypeClass = `message_${message.direction}_bubble`;
+        const messageTypeClass = `message_bubble ${message.direction}`;
         return (
-            <div key={key} className={`message ${messageTypeClass}`}>
-                <p className='h7 messageText'>{`${message.direction}, ${message.content}`}</p>
+            <div key={key} className={`messageContainer $${messageTypeClass}`}>
+                <div className={`message ${messageTypeClass}`}>
+                    <p className='h7 messageText'>{`${message.direction}, ${message.content}`}</p>
+                </div>
                 <p className='messageDate'>{`${message.date}`}</p>
             </div>
         )
