23a7596b4243e2b94b5e37f3471572c23c495280
Claudio De Angelis <claudio.deangelis@cloud-iq.com>
2018-10-08 22:00:02 +1100

    wrapped dbConnection in try-catch

--- src/server/controllers/dbConnection.js
+++ src/server/controllers/dbConnection.js
@@ -1,83 +1,92 @@
 const mongoUtil = require('./mongoUtil');
 
-let mongoClient,db;
+let mongoClient, db;
 
-exports.handleConnection = (req,res) => {
+exports.handleConnection = (req, res) => {
     // console.log(req);
     const connectionType = req.body.connectionType;
-        switch (connectionType) {
-            case 'connect':
-                console.log('connect');
-                dbConnection(req,res);
-                break;
-            case 'disconnect':
-                dbClose(res);
-                break;
-            default:
-                break;
-        }
-};
-
-dbConnection = (req, res) => {
-    const params = req.body.params;
-    console.log(req);
-    if (!params.connection.db) {
-        return res.json({
-            status: 'Error',
-            message: `No DB specified`,
-            warning: '',
-            Collections: [],
-            isConnected: false
-        });
-    } else {
-        // console.log(params);
-        mongoClient = mongoUtil.connectToDB(params,(err)=>{
-            if(err){
-                return res.json({
-                    status: 'Error',
-                    message:`Server Error: ${err}.`,
-                    warning: '',
-                    Collections: [],
-                    isConnected: false
-                });
-            } else {
-                db = mongoUtil.getDB();
-                const hostName = mongoUtil.getHostName();
-                const mongoPort = mongoUtil.getMongoPort();
-                const dbName = mongoUtil.getDBName();
-                db.listCollections().toArray((err, collections) => {
-                    // console.log(collections);
-                    if (collections.length > 0) {
-                        return res.json({
-                            status: 'Success',
-                            message: `Successfully connected to DB '${dbName}' on ${hostName}:${mongoPort}.`,
-                            warning: '',
-                            Collections: collections,
-                            isConnected: true
-                        })  
-                    }
-                    else {
-                        return res.json({
-                            status: 'Success',
-                            message: ` Successfully connected to '${dbName}' on ${hostName}:${mongoPort}\n`,
-                            warning: `Warning: DB '${dbName}' seems to be empty.`,
-                            Collections: collections,
-                            isConnected: true
-                        });
-                    }
-
-                })
-            }
-        });
-
+    switch (connectionType) {
+        case 'connect':
+            console.log('connect');
+            dbConnection(req, res);
+            break;
+        case 'disconnect':
+            dbClose(res);
+            break;
+        default:
+            break;
     }
 };
 
-dbClose = (res) =>{
+dbConnection = (req, res) => {
+    try {
+        const params = req.body.params;
+        console.log(req);
+        if (!params.connection.db) {
+            return res.json({
+                status: 'Error',
+                message: `No DB specified`,
+                warning: '',
+                Collections: [],
+                isConnected: false
+            });
+        } else {
+            // console.log(params);
+            mongoClient = mongoUtil.connectToDB(params, (err) => {
+                if (err) {
+                    return res.json({
+                        status: 'Error',
+                        message: `Server Error: ${err}.`,
+                        warning: '',
+                        Collections: [],
+                        isConnected: false
+                    });
+                } else {
+                    db = mongoUtil.getDB();
+                    const hostName = mongoUtil.getHostName();
+                    const mongoPort = mongoUtil.getMongoPort();
+                    const dbName = mongoUtil.getDBName();
+                    db.listCollections().toArray((err, collections) => {
+                        // console.log(collections);
+                        if (collections && collections.length > 0) {
+                            return res.json({
+                                status: 'Success',
+                                message: `Successfully connected to DB '${dbName}' on ${hostName}:${mongoPort}.`,
+                                warning: '',
+                                Collections: collections,
+                                isConnected: true
+                            })
+                        }
+                        else {
+                            return res.json({
+                                status: 'Success',
+                                message: ` Successfully connected to '${dbName}' on ${hostName}:${mongoPort}\n`,
+                                warning: `Warning: DB '${dbName}' seems to be empty.`,
+                                Collections: collections,
+                                isConnected: true
+                            });
+                        }
+
+                    })
+                }
+            });
+        }
+    } catch (err) {
+        return res.json({
+            status: 'Error',
+            message: `Server Error: ${err}.`,
+            warning: '',
+            Collections: [],
+            isConnected: false
+        })
+    }
+};
+
+dbClose = (res) => {
     mongoClient = mongoUtil.getClient();
     mongoClient.close();
     console.log('closing connection to MongoDB...');
-    mongoClient.close(()=> {
+    mongoClient.close(() => {
         console.log('MongoDB connection closed');
         return res.json({
             status: 'Disconnected',
